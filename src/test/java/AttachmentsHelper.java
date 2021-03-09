4e6957b291d1d17d3f9f97d1ef804426a83b47a5
Erofeev, Artem <aerofeev@axiomsl.com>
2021-03-09 11:30:59 +0300

    little fixes

--- src/test/java/AttachmentsHelper.java
+++ src/test/java/AttachmentsHelper.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class AttachmentsHelper {
-    @Attachment(value = "{attachName}", type = "img/png")
+    @Attachment(value = "{attachName}", type = "image/png")
     public static byte[] attachScreenshot(String attachName) {
         return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
